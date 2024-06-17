1c950963d63c32b97d6a378774c8fbcd7573503c
salamanca <1105403463@qq.com>
2024-06-17 20:59:52 +0800

    first commit

---
+++ my-movies-demo/src/main/java/com/example/mymoviesdemo/MyMoviesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymoviesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoviesDemoApplication.class, args);
+    }
+
+}
